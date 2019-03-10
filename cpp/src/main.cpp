2aa8673792b7247c86e121cfdcb360f28e39ee74
DrXVII <egined@gmail.com>
2019-03-10 08:31:47 +0200

    beginnings of command processor

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -83,6 +83,8 @@ int main()
         std::cout << "\n> ";
         std::getline(std::cin, command);
 
+        process_command(command);
+
         //supporting only one word commands for this experiment
         auto action = world.m_one_obj_actions.find(command);
         if(action != world.m_one_obj_actions.end()) {
@@ -92,4 +94,39 @@ int main()
     }
 
     return 0;
+}
+
+//TODO this should go to utils.cpp or something
+std::string get_nth_word(const std::string& str, const std::string& sep, size_t n = 1) {
+    if(str.size() == 0) {return "";}
+
+    size_t current_word {0};
+    size_t beg_pos {0};
+    size_t ed_pos {0};
+
+    while(current_word < n) {
+        ++current_word;
+        beg_pos = (current_word > 1)? ed_pos + 1 : 0;
+        ed_pos = str.find_first_of(sep, beg_pos);
+        if(current_word == n) {
+            if(ed_pos == std::string::npos) {return str.substr(beg_pos);}
+            else {return str.substr(beg_pos, ed_pos - beg_pos);}
+        }
+    }
+
+    return "";
+}
+
+void process_command(const std::string& command)
+{
+    /*algorhithm logic:
+     * [typical action signature '<verb> [article] <noun> [<connector> [article] <noun>]
+     * check for aliases
+     * check first word to determine candidate list of actions
+     * check remaining string against local objects (pick longest match)
+     * if there's more, check what's left against second word of function(s) in candidate list
+     * what's left is our second object - find that among local objects
+    */
+
+    std::cerr << "first word: " << get_nth_word(command, " ") << std::endl;
 }
